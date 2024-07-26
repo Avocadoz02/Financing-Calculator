0d36f4ee1bce8a8161cdf409ed6a5fd8a624568b
Kasidit Chaipech <benkasidit2945@gmail.com>
2024-07-26 09:24:13 +0700

    connect to html (Process)

--- finance.js
+++ finance.js
@@ -21,18 +21,22 @@ for (let i = 0; i < Year.length; i++ ){
     console.log("ค่างวด " + Year[i]*12 + " เดือน " + Payment.toFixed(2) + " บาท")
 }
 */
-const Price = 399000, Discount = 12000, Interest = 0.0349;
+const Price = 399000, Discount = 12000, Interest = 3.49/100;
 const DownCash = Price * 0.4 - Discount;
 const Finance = Price - DownCash - Discount;
 const FinanceInterest = Finance * Interest;
+
+function PaymentFunc(){
+    [2, 3, 4].forEach(year => {
+        const AllInterest = FinanceInterest * year;
+        const AllFinance = Finance + AllInterest;
+        const Payment = AllFinance / (year * 12);
+        console.log(`ค่างวด ${year * 12} เดือน ${Payment.toFixed(2)} บาท`);
+    });
+}
 
 console.log(`เงินดาวน์ทั้งหมด = ${DownCash} บาท`);
 console.log(`ยอดจัดไฟแนนซ์ = ${Finance} บาท`);
 console.log(`ยอดจัด * ดอกเบี้ย = ${FinanceInterest} บาท/ปี`);
 
-[2, 3, 4].forEach(year => {
-    const AllInterest = FinanceInterest * year;
-    const AllFinance = Finance + AllInterest;
-    const Payment = AllFinance / (year * 12);
-    console.log(`ค่างวด ${year * 12} เดือน ${Payment.toFixed(2)} บาท`);
-});
+PaymentFunc();
