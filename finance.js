11cce59691f7a93de193dd920f26fae6ceb9a23b
Kasidit Chaipech <benkasidit2945@gmail.com>
2024-08-05 09:15:46 +0700

    Get Value
    - Get Value from Input

--- finance.js
+++ finance.js
@@ -21,24 +21,36 @@ for (let i = 0; i < Year.length; i++ ){
     console.log("ค่างวด " + Year[i]*12 + " เดือน " + Payment.toFixed(2) + " บาท")
 }
 */
-const Price = document.getElementById('price').value
-const Discount = 12000
-const Interest = 3.49/100;
-const DownCash = Price * 0.4 - Discount;
-const Finance = Price - DownCash - Discount;
-const FinanceInterest = Finance * Interest;
+
 
 function PaymentFunc(){
+    const PriceText = document.getElementById('price').value
+    const Price = parseInt(PriceText.replace(/,/g, ''), 10)
+    const PerDown = document.getElementById('percentdowncash').value
+    const DiscountText = document.getElementById('discount').value
+    const Discount = parseInt(DiscountText.replace(/,/g, ''), 10)
+    const InterestInput = document.getElementById('interest').value;
+    const Interest = InterestInput/100;
+    const DownCash = Price * (PerDown/100) - Discount;
+    const Finance = Price - DownCash - Discount;
+    const FinanceInterest = Finance * Interest;
+
+    console.log(`เงินดาวน์ทั้งหมด = ${DownCash} บาท`);
+    console.log(`ยอดจัดไฟแนนซ์ = ${Finance} บาท`);
+    console.log(`ยอดจัด * ดอกเบี้ย = ${FinanceInterest} บาท/ปี`);
+
     [2, 3, 4].forEach(year => {
         const AllInterest = FinanceInterest * year;
         const AllFinance = Finance + AllInterest;
         const Payment = AllFinance / (year * 12);
         console.log(`ค่างวด ${year * 12} เดือน ${Math.ceil(Payment)} บาท`);
     });
+
+    return false;
 }
 
-console.log(`เงินดาวน์ทั้งหมด = ${DownCash} บาท`);
-console.log(`ยอดจัดไฟแนนซ์ = ${Finance} บาท`);
-console.log(`ยอดจัด * ดอกเบี้ย = ${FinanceInterest} บาท/ปี`);
+// console.log(`เงินดาวน์ทั้งหมด = ${DownCash} บาท`);
+// console.log(`ยอดจัดไฟแนนซ์ = ${Finance} บาท`);
+// console.log(`ยอดจัด * ดอกเบี้ย = ${FinanceInterest} บาท/ปี`);
 
 // PaymentFunc();
